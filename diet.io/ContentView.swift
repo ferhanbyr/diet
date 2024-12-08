d4324c0e38c762f83537eb4aae80d9f4c8bccefa
Ferhan BAYIR <ferhanbayir1@gmail.com>
2024-12-08 18:11:41 +0300

    Initial Commit

---
+++ diet.io/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  diet.io
+//
+//  Created by Ferhan Bayır on 8.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ diet.io/diet_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  diet_ioApp.swift
+//  diet.io
+//
+//  Created by Ferhan Bayır on 8.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct diet_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
